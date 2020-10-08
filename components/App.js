ada7f6ca11c2b1d5ae224db9e33273cb43dcf152
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-10-08 16:01:07 +0300

    Created some files and a folder for this project and rename the exist index.html file into something else

---
+++ components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ButtonComponent from "../components/ButtonComponent";
+
+function App() {
+    return (
+        <div>
+            <ButtonComponent className="default"/>
+            <ButtonComponent/>
+        </div>
+    )
+}
+
+export default App;
